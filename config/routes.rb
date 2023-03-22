b886e76002c3befa68efb95b3de4eee05533b09b
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-03-22 12:09:33 +0000

    updated routes with new way of logging in

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   # This line overrides the registration controller of Devise (so that it'll be a single user app)
   devise_for :users, controllers: { registrations: "registrations" }
+
+  devise_scope :user do
+    get "/entrar", to: "devise/sessions#new"
+  end
 end
