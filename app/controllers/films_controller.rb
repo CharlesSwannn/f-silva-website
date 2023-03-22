225aa3668f8dd73b7387d1a70bd494730d99491d
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-03-22 12:36:44 +0000

    created form for movie creation

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -2,4 +2,23 @@ class FilmsController < ApplicationController
   def index
     @films = Film.all
   end
+
+  def new
+    @film = Film.new
+  end
+
+  def create
+    @film = Film.create(film_params)
+    if @film.save
+      redirect_to films_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def film_params
+    params.require(:film).permit(:title, :description, :year, :festivals, :awards)
+  end
 end
