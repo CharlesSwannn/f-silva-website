91a50b17638ae6ac6ecee53fdfa855afe2a6b9d6
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-03-23 18:45:39 +0000

    created routes for commercial

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin
   root to: "pages#home", as: :home
   resources :films
+  resources :commercials
 
 
   # This line overrides the registration controller of Devise (so that it'll be a single user app)
