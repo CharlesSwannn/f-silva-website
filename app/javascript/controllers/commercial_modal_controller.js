3c2605b8530d67e72fa3027e0065167f3165dc05
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-04-03 16:08:32 +0100

    finished the modal stimulus controller

--- app/javascript/controllers/commercial_modal_controller.js
+++ app/javascript/controllers/commercial_modal_controller.js
@@ -2,6 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="commercial-modal"
 export default class extends Controller {
+  static targets = ["image"]
   connect() {
+    console.log("yo, mf!")
+  }
+  largeImage(event){
+    if(event.target.classList.contains("modal-photo")){
+      const src = event.target.getAttribute("src");
+      this.imageTarget.src = src;
+      const myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+      myModal.show();
+    }
   }
 }
