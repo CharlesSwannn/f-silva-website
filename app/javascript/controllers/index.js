7783ce5a2383b409fc34693fd02a6b4effb3bb60
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-04-03 14:33:45 +0100

    created stimulus controller to control show modal

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import CommercialModalController from "./commercial_modal_controller"
+application.register("commercial-modal", CommercialModalController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
