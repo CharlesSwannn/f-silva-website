09d4827b0580d7109c1255d2d33c773eadff5a80
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-04-10 16:31:16 +0100

    removed texts table from db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_135216) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_153020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,15 +89,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_135216) do
     t.string "director"
     t.string "producer"
     t.integer "category", default: 0
-  end
-
-  create_table "texts", force: :cascade do |t|
-    t.string "type"
-    t.string "title"
-    t.string "content"
-    t.string "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
